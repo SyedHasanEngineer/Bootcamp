a6345e6212c39aa5b75c5294aad09964e503cf00
mirzaimran-code <mirzaimranbaig0123@gmail.com>
2025-01-25 20:55:03 -0600

    Added a function that can now multiply three numbers

--- calculator_project.py
+++ calculator_project.py
@@ -1,4 +1,5 @@
 from ron_addition import add_3
+from MUL_Imran import mul_3
 def add(a, b):
     return a + b
 
@@ -19,11 +20,12 @@ print("2. Subtraction of 2 numbers")
 print("3. Multiplication of 2 numbers")
 print("4. Division of 2 numbers")
 print("5. Addition of 3 numbers")
+print("6. Multiplication of 3 numbers")
 
-choice = int(input("Enter your choice (1-5): "))
+choice = int(input("Enter your choice (1-6): "))
 num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
-if choice==5: 
+if choice==5 or choice==6: 
     num3 = float(input("Enter third number: "))
 
 if choice == 1:
@@ -36,6 +38,8 @@ elif choice == 4:
     result = div(num1, num2)
 elif choice == 5:
     result = add_3(num1, num2,num3)
+elif choice ==6:
+    result = mul_3(num1, num2,num3)
 else:
     result = "Error: Invalid choice!"
 
